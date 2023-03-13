b86d833ae3b1cca94025741f77937f3a97b34377
insurace-yuxuan <yuxuan.wong@mtrlabs.com>
2023-03-13 15:26:23 +0800

    Add Get Premium

--- example/src/view/BuyCoverPage.tsx
+++ example/src/view/BuyCoverPage.tsx
@@ -1,5 +1,5 @@
 import { BN, ProductId } from '@amulet/sdk';
-import { useCallback, useState } from 'react';
+import { useCallback, useMemo, useState } from 'react';
 import { useAmulet, useBlockchain } from '../hook';
 
 export default function BuyCoverPage() {
@@ -10,8 +10,55 @@ export default function BuyCoverPage() {
   const [signature, setSignature] = useState('');
   const [error, setError] = useState<any>(null);
 
+  const [inputProductId, setInputProductId] = useState('1');
+  const [inputAmount, setInputAmount] = useState('1');
+  const [inputDays, setInputDays] = useState('15');
+
+  const [premium, setPremium] = useState(new BN(0));
+
+  const productId = useMemo(() => {
+    return Number(inputProductId);
+  }, [inputProductId]);
+
+  const coverAmount = useMemo(() => {
+    return new BN(Number(inputAmount) * 1e9);
+  }, [inputAmount]);
+
+  const days = useMemo(() => {
+    return Number(inputDays);
+  }, [inputDays]);
+
+  const handleGetPremium = useCallback(async () => {
+    setSignature('');
+    setError(null);
+
+    if (publicKey == null || provider == null) {
+      return;
+    }
+
+    setLoading(true);
+
+    try {
+      const result = await amulet.getPremium({
+        provider,
+        wallet: publicKey,
+        productId,
+        coverToken: amulet.tokens.auwt.publicKey,
+        coverAmount,
+        days,
+      });
+
+      setPremium(result.premium);
+    } catch (e) {
+      setError(e);
+    }
+
+    setLoading(false);
+  }, [amulet, publicKey, provider, productId, coverAmount, days]);
+
   const handleBuyCover = useCallback(async () => {
     setSignature('');
+    setError(null);
 
     if (publicKey == null || provider == null) {
       return;
@@ -24,10 +71,10 @@ export default function BuyCoverPage() {
         provider,
         owner: publicKey,
         referrer: publicKey,
-        productId: ProductId.Solend,
+        productId,
         coverToken: amulet.tokens.auwt.publicKey,
-        coverAmount: new BN(1e9), // 1 aUWT
-        days: 15,
+        coverAmount,
+        days,
       });
 
       const result = await sendAndConfirmTransaction(transaction, signers);
@@ -38,11 +85,34 @@ export default function BuyCoverPage() {
     }
 
     setLoading(false);
-  }, [amulet, publicKey, provider, sendAndConfirmTransaction]);
+  }, [amulet, publicKey, provider, sendAndConfirmTransaction, productId, coverAmount, days]);
 
   return (
     <div>
       <h1>Buy Cover</h1>
+      <p>Product ID</p>
+      <p>
+        <input value={inputProductId} onChange={(event) => setInputProductId(event.target.value)} />
+      </p>
+      <p>
+        {
+          Object.entries(ProductId).map((([name, id]) => (
+            <div key={id}>{ id } { name }</div>
+          )))
+        }
+      </p>
+      <p>Cover Amount</p>
+      <p>
+        <input value={inputAmount} onChange={(event) => setInputAmount(event.target.value)} />
+        <span> aUWT</span>
+      </p>
+      <p>Cover Period</p>
+      <p>
+        <input value={inputDays} onChange={(event) => setInputDays(event.target.value)} />
+        <span> day(s)</span>
+      </p>
+      <p>Premium: { (premium.toNumber() / 1e9).toFixed(4) } SOL</p>
+      <button onClick={handleGetPremium}>Get Premium</button>
       <button onClick={handleBuyCover}>Buy Cover</button>
       { loading && <p>Loading...</p> }
       { (signature !== '') && <p>Transaction Successful. Signature: { signature }</p> }
